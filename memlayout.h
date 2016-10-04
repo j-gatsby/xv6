5acb2f18ef63700b76c138950f71f24d9ec3023a
j-gatsby <timjoeckel@gmail.com>
2016-10-03 23:48:35 -0500

    Add memlayout.h

---
+++ memlayout.h
@@ -0,0 +1,18 @@
+// Memory Layout
+
+#define EXTMEM		0x100000		// Start of extended memory
+#define PHYSTOP		0xE000000		// Top physical memory
+#define DEVSPACE	0xFE000000		// Other devices are at high addresses
+
+// Key addresses for address space layout
+// (see kmap in vm.c for layout)
+#define KERNBASE	0x8000000			// First kernel virtual address
+#define KERNLINK	(KERNBASE+EXTMEM)	// Address where kernel is linked
+
+// Macros to map virtual memory to and from physical memory
+#define V2P(a) (((uint) (a)) - KERNBASE)
+#define P2V(a) (((void *) (a)) + KERNBASE)
+
+#define V2P_WO(x) ((x) - KERNBASE)		// same as V2P, but without casts
+#define P2V_WO(x) ((x) + KERNBASE)		// same as P2V, but without casts
+
