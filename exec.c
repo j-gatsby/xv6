cd195153c17cbe335d961ef6a1182ad910a90234
j-gatsby <timjoeckel@gmail.com>
2016-10-21 20:29:45 -0500

    Add comments to exec.c

--- exec.c
+++ exec.c
@@ -1,3 +1,5 @@
+// exec() is the system call that creates the user part of the address space.
+
 #include "types.h"
 #include "param.h"
 #include "memlayout.h"
@@ -19,6 +21,9 @@ exec(char *path, char **argv)
 	pde_t *pgdir, *oldpgdir;
 
 	begin_op();
+	// Initalize the user part of the address space
+	// from a file stored in the file system.
+	// Open the named binary (path).
 	if ((ip = namei(path)) == 0)
 	{
 		end_op();
@@ -32,7 +37,7 @@ exec(char *path, char **argv)
 		goto bad;
 	if (elf.magic != ELF_MAGIC)
 		goto bad;
-
+	// Allocate a new page table with no user mappings
 	if ((pgdir = setupkvm()) == 0)
 		goto bad;
 
@@ -48,10 +53,12 @@ exec(char *path, char **argv)
 			goto bad;
 		if (ph.vaddr + ph.memsz < ph.vaddr)
 			goto bad;
+		// Allocate memory for each ELF segment
 		if ((sz = allocuvm(pgdir, sz, ph.vaddr + ph.memsz)) == 0)
 			goto bad;
 		if (ph.vaddr % PGSIZE != 0)
 			goto bad;
+		// Load each segment into memory
 		if (loaduvm(pgdir, (char*)ph.vaddr, ip, ph.off, ph.filesz) < 0)
 			goto bad;
 	}
@@ -60,28 +67,41 @@ exec(char *path, char **argv)
 	ip = 0;
 
 	// Allocate two pages at the next page boundary.
-	// Make the first inaccessible.
 	// Use the second as the user stack.
 	sz = PGROUNDUP(sz);
 	if ((sz = allocuvm(pgdir, sz, sz + 2*PGSIZE)) == 0)
 		goto bad;
+	// Make the first inaccessible.
+	// This will be the guard page, right below the stack.
+	// This is used to guard a stack growing off of the stack page.
+	// The guard page is not mapped, and so if the stack does run
+	// off of the stack page, the hardware will generate an
+	// exception because it cannot translate the faulting address.
 	clearpteu(pgdir, (char*)(sz - 2*PGSIZE));
 	sp = sz;
 
-	// Push argument strings, prepare rest of stack in ustack
+	// Push argument strings, prepare rest of stack in ustack.
 	for (argc = 0; argv[argc]; argc++)
 	{
 		if (argc >= MAXARG)
 			goto bad;
+		// Copy the argument strings to the top of the stack, one
+		// at a time,
 		sp = (sp - (strlen(argv[argc]) + 1)) & ~3;
 		if (copyout(pgdir, sp, argv[argc], strlen(argv[argc]) + 1) < 0)
 			goto bad;
+		// Record the pointers to the arg strings in ustack[],
+		// after skipping over the first three slots, which
+		// will be filled in below.
 		ustack[3+argc] = sp;
 	}
+	// Place a null pointer at the end of what will be
+	// the argv list passed to main().
 	ustack[3+argc] = 0;
 
-	ustack[0] = 0xffffffff;		// fake return PC
-	ustack[1] = argc;
+	// Record the first three entries in ustack[]
+	ustack[0] = 0xffffffff;			// fake return PC
+	ustack[1] = argc;				// arg count
 	ustack[2] = sp - (argc+1)*4;	// argv pointer
 
 	sp -= (3+argc+1) * 4;
@@ -102,10 +122,15 @@ exec(char *path, char **argv)
 	proc->tf->eip = elf.entry;		// main
 	proc->tf->esp = sp;
 	switchuvm(proc);
+	// exec() must wait until it is sure that the system
+	// call will succeed before it can free the old image.
 	freevm(oldpgdir);
 
 	return 0;
 
+// If exec() detects an error, like an invalid program segment,
+// we jump here, free the new image, and return -1.
+// The only error cases happen during the creation of the image.
 bad:
 	if (pgdir)
 		freevm(pgdir);
